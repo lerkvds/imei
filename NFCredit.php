0f5c5eed44e9142a332a7c6bbb3e58dd6224c130
lerkvds <76712337+lerkvds@users.noreply.github.com>
2020-12-29 18:32:44 +0700

    Update NFCredit.php

--- NFCredit.php
+++ NFCredit.php
@@ -1,9 +1,8 @@
 <?php
 
-$__uid = 'c'.mt_rand(10, 99).'e-e'.mt_rand(10, 99).'1-i'.mt_rand(10, 99).'k-a' . mt_rand(100, 999);
+$__uid = 'ab2d-e'.mt_rand(10, 99).'1-i'.mt_rand(10, 99).'k-a' . mt_rand(100, 999);
 $__imei = '86622805';
 $__length = 7;
-$__region = '';
 
 function http_request($url) {
     $ch = curl_init();
@@ -25,63 +24,47 @@ function randNumber($length) {
 
 $__stopped=false;$ii=0;
 
-echo 'NetflixCredit - Code by JamvanHax0r'. PHP_EOL;
+echo 'NetflixCredit - Coded by JamvanHax0r'. PHP_EOL;
 echo 'Support: MY TH PH'. PHP_EOL;
 echo '-----------------'. PHP_EOL;
 sleep(1);
-echo '> Masukkan UID (bebas): ';
+echo 'Input UID (optional): ';
 $input = fopen("php://stdin","r");
 $answer = trim(fgets($input));
 if($answer!=='') {
 	$__uid=$answer;
 }
 sleep(1);
-echo '> Masukkan awalan IMEI ('.$__imei.'): ';
+echo 'Input awalan IMEI ('.$__imei.'): ';
 $input = fopen("php://stdin","r");
 $answer = trim(fgets($input));
 if($answer!=='') {
 	$__imei=$answer;
 }
 sleep(1);
-echo '> Masukkan panjang IMEI ('.$__length.'): ';
+echo 'Input panjang random IMEI ('.$__length.'): ';
 $input = fopen("php://stdin","r");
 $answer = trim(fgets($input));
 if($answer!=='') {
 	$__length=(int)$answer;
 }
+echo '[x] UID anda: ' . $__uid . PHP_EOL;
+echo '[x] Awalan IMEI: ' . $__imei . PHP_EOL;
+echo '[x] Panjang random IMEI: ' . $__length . PHP_EOL;
 sleep(1);
+echo 'Input captcha akan muncul 3x.' . PHP_EOL;
+echo 'Buka link dibawah ini, lalu input captchanya.' . PHP_EOL;
+
 $__setting = [
 	['my', '', 'Malaysia', 'https://hd.c.mi.com/my/eventapi/api/aptcha/index?type=netflix&uid='.$__uid],
 	['th', '', 'Thailand', 'https://hd.c.mi.com/th/eventapi/api/aptcha/index?type=netflix&uid='.$__uid],
 	['ph', '', 'Philippines', 'https://hd.c.mi.com/ph/eventapi/api/aptcha/index?type=netflix&uid='.$__uid]
 ];
-echo '(0) '.$__setting[0][2] . PHP_EOL;
-echo '(1) '.$__setting[1][2] . PHP_EOL;
-echo '(2) '.$__setting[2][2] . PHP_EOL;
-echo '> Input Region (Enter for all region): ';
-$input = fopen("php://stdin","r");
-$answer = trim(fgets($input));
-if($answer=='') {
-	$__region='All region';
-} else {
-	$__setting_temp=$__setting;
-	$__setting=[];
-	$__setting[]=$__setting_temp[(int)$answer];
-	$__region=$__setting[0][2];
-}
-echo '[x] UID ente: ' . $__uid . PHP_EOL;
-echo '[x] Awalan IMEI: ' . $__imei . PHP_EOL;
-echo '[x] Panjang random IMEI: ' . $__length . PHP_EOL;
-echo '[x] Region: ' . $__region . PHP_EOL;
-sleep(1);
-echo 'Input captcha akan muncul 3x.' . PHP_EOL;
-echo 'Buka link dibawah ini, lalu input captchanya.' . PHP_EOL;
 
-sleep(1);
 foreach ($__setting as $sett) {
 	echo '[' . $sett[2] . '] Challenge the captcha' . PHP_EOL;
 	echo '=> ' . $sett[3] . PHP_EOL;
-	echo '> Input captcha : ';
+	echo 'Input captcha : ';
 	$input = fopen("php://stdin","r");
 	$answer = trim(fgets($input));
 	$__setting[$ii][1] = $answer;
@@ -101,17 +84,12 @@ while(1) {
 		$data = json_decode($data, true);
 		if (isset($data['msg']) && $data['msg'] == 'Success') {
 			$__if_valid = $data['data']['redirect_url'] . '|' . $imei . '|' . $sett[2] . PHP_EOL;
-			$data_n = http_request($data['data']['redirect_url']);
-			if(strpos($data_n, 'has already been used')) {
-				echo 'USED => ' . $__if_valid;
-			} else {
-				file_put_contents("valid.txt", $__if_valid, FILE_APPEND);
-				echo 'FRESH => ' . $__if_valid;
-			}
+			file_put_contents("valid.txt", $__if_valid, FILE_APPEND);
+			echo 'VALID => ' . $__if_valid;
 		} else if(isset($data['code']) && $data['code'] == '800706') {
 			echo '[' . $sett[2] . '] Challenge the captcha again' . PHP_EOL;
 			echo '=> ' . $sett[3] . PHP_EOL;
-			echo '> Input new captcha : ';
+			echo 'Input captcha : ';
 			$input = fopen("php://stdin","r");
 			$answer = trim(fgets($input));
 			$__setting[$ii][1] = $answer;
